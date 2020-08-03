629106c43f2281600ba256411409103f8f2e0283
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2020-08-03 09:05:11 -0400

    fix issue with tree cut mapping

--- R/EpivizTreeData-class.R
+++ R/EpivizTreeData-class.R
@@ -560,7 +560,7 @@ EpivizTreeData$methods(
     measurements <-  metadata(.self$.object)$tsne
     
     data <- list()
-    level<- .self$.levelSelected
+    level<- .self$.levelSelected + 1
     i<- 1
     for (col in rownames(metadata(.self$.object)$tsne)) {
       
@@ -609,7 +609,7 @@ EpivizTreeData$methods(
     }
 
     if(is.null(selectedLevels)) {
-      selectedLevels = .self$.levelSelected
+      selectedLevels = .self$.levelSelected + 1
     }
     
     selections = .self$.nodeSelections
