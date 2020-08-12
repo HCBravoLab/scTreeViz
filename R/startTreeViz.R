c1a74b2db82a7082df0e34a41a3cef8e7b933975
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2020-08-12 16:03:17 -0400

    add top_genes as a parameter to startTreeViz

--- R/startTreeViz.R
+++ R/startTreeViz.R
@@ -1,9 +1,9 @@
 .register_all_treeviz_things <- function(app) {
-
+  
   app$server$register_action("registerChartTypes", function(request_data) {
     app$chart_mgr$.register_available_chart_types(request_data$data)
   })
-
+  
   app$server$register_action("getHierarchy", function(request_data) {
     datasource = request_data$datasourceGroup
     nodeId = request_data$nodeId
@@ -13,52 +13,52 @@
     }
     obj$getHierarchy(nodeId)
   })
-
+  
   app$server$register_action("propagateHierarchyChanges", function(request_data) {
-
+    
     datasource = request_data$datasourceGroup
     selection = request_data$selection
     order = request_data$order
     selectedLevels <- request_data$selectedLevels
-
+    
     obj <- app$data_mgr$.find_datasource(datasource)
     if (is.null(obj)) {
       stop("cannot find datasource", datasource)
     }
-
+    
     obj$propagateHierarchyChanges(selection, order, selectedLevels)
   })
-
+  
   app$server$register_action("getRows", function(request_data) {
     datasource <- request_data$datasource
     seqName <- request_data$seqName
     start <- request_data$start
     end <- request_data$end
     metadata <- request_data$metadata
-
+    
     obj <- app$data_mgr$.find_datasource(datasource)
     if (is.null(obj)) {
       stop("cannot find datasource", datasource)
     }
     obj$getRows(seqName, start, end, metadata)
   })
-
+  
   app$server$register_action("getValues", function(request_data) {
     datasource <- request_data$datasource
     measurement <- request_data$measurement
     seqName <- request_data$seqName
     start <- request_data$start
     end <- request_data$end
-
+    
     obj <- app$data_mgr$.find_datasource(datasource)
     if (is.null(obj)) {
       stop("cannot find datasource", datasource)
     }
     obj$getValues(measurement, seqName, start, end)
   })
-
+  
   app$server$register_action("getCombined", function(request_data) {
-
+    
     measurementsList <- request_data$measurements
     result <- lapply(names(measurementsList), function(m) {
       seqName <- request_data$seqName
@@ -68,21 +68,21 @@
       nodeSelection <- request_data$selection
       selectedLevels <- request_data$selectedLevels
       measurements <- measurementsList[[m]]
-
+      
       if(is.null(start)) {
         start <- 0
       }
       else {
         start <- start
       }
-
+      
       if(is.null(end)) {
         end <- 100000
       }
       else {
         end <- end
       }
-
+      
       obj <- app$data_mgr$.find_datasource(m)
       if (is.null(obj)) {
         stop("cannot find datasource", m)
@@ -96,7 +96,7 @@
     names(result) <- names(measurementsList)
     result
   })
-
+  
   app$server$register_action("splinesSettings", function(request_data) {
     updateAlpha <- as.double(request_data$settings$alpha)
     names_list <- ls(app$data_mgr$.ms_list)
@@ -105,21 +105,21 @@
     obj$updateSplineAlpha(updateAlpha)
     return(list())
   })
-
+  
   app$server$register_action("getSeqInfos", function(request_data) {
     return(list(
       "treevizr" = c(0, 100000)
     ))
   })
-
+  
   app$server$register_action("partitions", function(request_data) {
     return(list(
       "treevizr" = c(0, 100000)
     ))
   })
-
+  
   app$server$register_action("getPCA", function(request_data) {
-
+    
     obj <- app$data_mgr$.find_datasource(request_data$measurements)
     if (is.null(obj)) {
       stop("cannot find datasource", request_data$measurements)
@@ -128,16 +128,16 @@
     result <- list(data = result)
     names(result) <- request_data$measurements
     result
-
+    
   })
-
+  
   app$server$register_action("getDiversity", function(request_data) {
-
+    
     measurementsList <- request_data$measurements
     result <- lapply(names(measurementsList), function(m) {
       seqName <- request_data$seqName
       measurements <- measurementsList[[m]]
-
+      
       obj <- app$data_mgr$.find_datasource(m)
       if (is.null(obj)) {
         stop("cannot find datasource", m)
@@ -146,19 +146,19 @@
     })
     names(result) <- names(measurementsList)
     result
-
+    
   })
-
+  
   app$server$register_action("search", function(request_data) {
     query <- request_data$q
     max_results <- request_data$maxResults
     datasource = request_data$datasourceGroup
-
+    
     obj <- app$data_mgr$.find_datasource(datasource)
     if (is.null(obj)) {
       stop("cannot find datasource", datasource)
     }
-
+    
     list(nodes = obj$searchTaxonomy(query, max_results))
   })
   
@@ -217,70 +217,110 @@
 #' app$stop_app()
 #'
 #' @export
-startTreeviz <- function(data = NULL, host="http://epiviz.cbcb.umd.edu/treeviz",
-                         register_function = .register_all_treeviz_things,
-                         ...) {
-  chr="treevizr"
-  start <- 0
-  end <- 100000
-  app <- startEpiviz(host = host, register_function = register_function,
-                     chr=chr, start=start, end=end, ...)
-  mApp <- TreeVizApp$new(.url_parms=app$.url_parms, .browser_fun=app$.browser_fun,
-                         server=app$server, data_mgr=app$data_mgr, chart_mgr=app$chart_mgr)
-
-
-  # if (is(data, "Seurat")) {
-  #   treeViz <- createFromSeurat(data)
-  # }
-  # else if (is(data, "SingleCellExperiment")) {
-  #   treeViz <- createFromSCE(data)
-  # }
-
+startTreeviz <- function(data = NULL, top_genes=100, host="http://epiviz.cbcb.umd.edu/treeviz",
+                         http_port=NULL, path=NULL, use_devel=FALSE, 
+                         chr="treevizr", start=0, end=100000, 
+                         debug=FALSE, workspace=NULL, scripts=NULL, gists=NULL, use_cookie=TRUE,
+                         register_function=.register_all_treeviz_things,
+                         open_browser=TRUE, server=NULL, browser_fun=utils::browseURL, 
+                         ws_host="localhost", ...) {
+  
+  if(is.null(server) || !is(server, "EpivizServer") ) {
+    server <- epivizrServer::createServer(...)
+  }
+  
+  data_mgr <- TreeVizDataMgr$new(server = server)
+  chart_mgr <- TreeVizChartMgr$new(server=server)
+  
+  url_parms <- list(host=host,
+                    http_port=http_port,
+                    path=path,
+                    ws_port=server$.port,
+                    use_devel=use_devel,
+                    debug=debug,
+                    chr=chr,
+                    start=start,
+                    end=end,
+                    workspace=workspace,
+                    scripts=scripts,
+                    gists=gists,
+                    use_cookie=use_cookie,
+                    ws_host=ws_host)
+  
+  app <- TreeVizApp$new(.url_parms=url_parms,
+                        .browser_fun=browser_fun,
+                        server=server,
+                        data_mgr=data_mgr,
+                        chart_mgr=chart_mgr)
+  
+  if (app$server$.verbose) {
+    cat("Starting TreeViz!\n")
+  }
+  
+  register_function(app)
+  
+  if (!app$server$is_interactive()) {
+    return(app)
+  }
+  
+  if (app$server$.verbose) {
+    cat("Initializing session manager...\n")
+  }
   tryCatch({
-
-    send_request <- mApp$server$is_interactive()
-    
-    if (mApp$server$is_interactive()) {
-      .wait_until_connected(mApp$server)
+    if (app$server$.verbose) {
+      cat("Opening connections...\n")
     }
-
+    
+    if (open_browser) {
+      app$.open_browser()
+    }
+    
+    if (app$server$.verbose) {
+      cat("Done starting Epivizr!\n")
+    }
+    
+    send_request <- app$server$is_interactive()
+    
+    if (app$server$is_interactive()) {
+      .wait_until_connected(app$server)
+    }
+    
     if (!is.null(data)) {
-
-      data <- find_top_variable_genes(data, 100)
+      
+      data <- find_top_variable_genes(data, top_genes)
       
       
-      mApp$navigate(chr, start, end)
-      mApp$server$wait_to_clear_requests()
-      .delay_requests(mApp$server)
-
+      app$navigate(chr, start, end)
+      app$server$wait_to_clear_requests()
+      .delay_requests(app$server)
+      
       # facetZoom
-      facetZoom <- mApp$data_mgr$add_measurements(data, datasource_name = "SCRNA", tree = "col", datasource_origin_name="scrna", send_request=send_request)
-      mApp$server$wait_to_clear_requests()
-      .delay_requests(mApp$server)
-
-      mApp$chart_mgr$plot(facetZoom, send_request=send_request)
-      mApp$server$wait_to_clear_requests()
-      .delay_requests(mApp$server)
-
+      facetZoom <- app$data_mgr$add_measurements(data, datasource_name = "SCRNA", tree = "col", datasource_origin_name="scrna", send_request=send_request)
+      app$server$wait_to_clear_requests()
+      .delay_requests(app$server)
+      
+      app$chart_mgr$plot(facetZoom, send_request=send_request)
+      app$server$wait_to_clear_requests()
+      .delay_requests(app$server)
+      
       # Heatmap
       ms_list <- facetZoom$get_measurements()
       subset_ms_list <- Filter(function(ms) ms@id %in% metadata(data)$top_variable, ms_list)
       
-      mApp$chart_mgr$visualize(chart_type = "HeatmapPlot",  measurements = subset_ms_list)
-      mApp$server$wait_to_clear_requests()
-      .delay_requests(mApp$server)
-
+      app$chart_mgr$visualize(chart_type = "HeatmapPlot",  measurements = subset_ms_list)
+      app$server$wait_to_clear_requests()
+      .delay_requests(app$server)
+      
       # TSNE
-      mApp$chart_mgr$revisualize(chart_type = "TSNEPlot", chart= facetZoom)
-      mApp$server$wait_to_clear_requests()
-
+      app$chart_mgr$revisualize(chart_type = "TSNEPlot", chart= facetZoom, mFlag=FALSE)
+      app$server$wait_to_clear_requests()
     }
+    
+    app
   }, error=function(e) {
-    mApp$stop_app()
-    stop(e)
-  })
-
-  mApp
+    app$stop_app()
+    stop("Error starting Epiviz: ", e)
+  }, interrupt=function(e) {NULL})  
 }
 
 
@@ -320,41 +360,41 @@ startTreevizStandalone <- function(data = NULL, register_function = .register_al
                  seqlengths=100000,
                  isCircular=FALSE,
                  genome="treevizr")
-
+  
   path <- system.file("www", package="epivizrStandalone")
-
+  
   app <- startStandalone(seqinfo=seq,
                          register_function=register_function,
                          use_viewer_option = use_viewer_option,
                          chr=chr, start=start, end=end, ...)
-
+  
   mApp <- TreeVizApp$new(.url_parms=app$.url_parms, .browser_fun=app$.browser_fun,
                          server=app$server, data_mgr=app$data_mgr, chart_mgr=app$chart_mgr)
   tryCatch({
-
+    
     send_request <- mApp$server$is_interactive()
     
     if (mApp$server$is_interactive()) {
       .wait_until_connected(mApp$server)
     }
-
+    
     if (!is.null(data)) {
-
+      
       data <- find_top_variable_genes(data, 100)
       
       mApp$navigate(chr, start, end)
       mApp$server$wait_to_clear_requests()
       .delay_requests(mApp$server)
-
+      
       # facetZoom
       facetZoom <- mApp$data_mgr$add_measurements(data, datasource_name = "SCRNA", tree = "col", datasource_origin_name="scrna", send_request=send_request)
       mApp$server$wait_to_clear_requests()
       .delay_requests(mApp$server)
-
+      
       mApp$chart_mgr$plot(facetZoom, send_request=send_request)
       mApp$server$wait_to_clear_requests()
       .delay_requests(mApp$server)
-
+      
       # Heatmap
       ms_list <- facetZoom$get_measurements()
       subset_ms_list <- Filter(function(ms) ms@id %in% metadata(data)$top_variable, ms_list)
@@ -362,17 +402,17 @@ startTreevizStandalone <- function(data = NULL, register_function = .register_al
       mApp$chart_mgr$visualize(chart_type = "HeatmapPlot",  measurements = subset_ms_list)
       mApp$server$wait_to_clear_requests()
       .delay_requests(mApp$server)
-
+      
       # TSNE
       mApp$chart_mgr$revisualize(chart_type = "TSNEPlot", chart= facetZoom)
       mApp$server$wait_to_clear_requests()
-
+      
     }
   }, error=function(e) {
     mApp$stop_app()
     stop(e)
   })
-
+  
   mApp
 }
 
